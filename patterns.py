855e0ded7b139f1b1b1b1efe6396eed6aa493368
Amela Špica <amela.spica@gmail.com>
2018-03-10 21:02:25 +0100

    patterns

--- patterns.py
+++ patterns.py
@@ -4,25 +4,35 @@ import math
 import numpy as np
 
 dataLeft = pd.read_csv('Merged/dataLeft.csv')
-changed = False
-for index, row in dataLeft.iterrows():
+#for index, row in dataLeft.iterrows():
+index = 0
+listOfTemperatures = []
+while index < len(dataLeft):
     listOfPatterns = []
-    listOfTemperatures = []
+    changed = False
     while changed is False:
         # check if cabine value has changed
         if index > 0:
-            if dataLeft.loc[index, 'cabine'] != dataLeft.loc[index-1, 'cabine']:
+            if dataLeft.loc[index, 'cabine'] != dataLeft.loc[index - 1, 'cabine']:
                 changed = True
                 break
-            else:
-                changed = False
         #write all elements to one list
         listOfPatterns.append(dataLeft.loc[index])
         listOfTemperatures.append(dataLeft.loc[index, 'real_temp'])
 
-        index = index + 1
+        if index < len(dataLeft):
+            index = index + 1
+
+    print("index " + str(index))
     print("list of temperatures")
     print(listOfTemperatures)
     #calculate min
     min_temp = min(listOfTemperatures)
+    print(min_temp)
+
+    listOfTemperatures = []
+    listOfTemperatures.append(dataLeft.loc[index, 'real_temp'])
+
+    if index < len(dataLeft):
+        index = index + 1
 
