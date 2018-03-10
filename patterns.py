530a369c5689983a874430415031ff976cca3d8d
Amela Špica <amela.spica@gmail.com>
2018-03-10 21:26:22 +0100

    grouping ref, rel temp and balcony

--- patterns.py
+++ patterns.py
@@ -7,13 +7,14 @@ dataLeft = pd.read_csv('Merged/dataLeft.csv')
 #for index, row in dataLeft.iterrows():
 index = 0
 listOfTemperatures = []
-print("size " + str(len(dataLeft)))
+listOfRelTemperatures = []
+listBalcony = []
+
 while index < len(dataLeft):
     listOfPatterns = []
     changed = False
     while changed is False:
         # check if cabine value has changed
-        print("index in if" + str(index) + "value " + str(dataLeft.loc[index, 'real_temp']))
         if index > 0 & index < len(dataLeft):
             if dataLeft.loc[index, 'cabine'] != dataLeft.loc[index - 1, 'cabine']:
                 changed = True
@@ -21,19 +22,22 @@ while index < len(dataLeft):
 
         #write all elements to one list
         listOfPatterns.append(dataLeft.loc[index])
+        listBalcony.append(dataLeft.loc[index, 'balcony'])
         listOfTemperatures.append(dataLeft.loc[index, 'real_temp'])
+        listOfRelTemperatures.append(dataLeft.loc[index, 'ref_temp'])
         if index >= (len(dataLeft) - 1):
             break
         if index <= len(dataLeft):
             index = index + 1
 
-    print("index " + str(index))
-    print("list of temperatures")
-    print(listOfTemperatures)
     #calculate min
     min_temp = min(listOfTemperatures)
-    print(min_temp)
+    #print(min_temp)
 
+    listBalcony = []
+    listBalcony.append(dataLeft.loc[index, 'balcony'])
+    listOfRelTemperatures = []
+    listOfRelTemperatures.append(dataLeft.loc[index, 'ref_temp'])
     listOfTemperatures = []
     listOfTemperatures.append(dataLeft.loc[index, 'real_temp'])
 
