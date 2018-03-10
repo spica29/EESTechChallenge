f32a270d05b90c1ac5bc80a02792b06e7487ba03
Amela Špica <amela.spica@gmail.com>
2018-03-10 21:19:34 +0100

    grouping works

--- patterns.py
+++ patterns.py
@@ -7,20 +7,24 @@ dataLeft = pd.read_csv('Merged/dataLeft.csv')
 #for index, row in dataLeft.iterrows():
 index = 0
 listOfTemperatures = []
+print("size " + str(len(dataLeft)))
 while index < len(dataLeft):
     listOfPatterns = []
     changed = False
     while changed is False:
         # check if cabine value has changed
-        if index > 0:
+        print("index in if" + str(index) + "value " + str(dataLeft.loc[index, 'real_temp']))
+        if index > 0 & index < len(dataLeft):
             if dataLeft.loc[index, 'cabine'] != dataLeft.loc[index - 1, 'cabine']:
                 changed = True
                 break
+
         #write all elements to one list
         listOfPatterns.append(dataLeft.loc[index])
         listOfTemperatures.append(dataLeft.loc[index, 'real_temp'])
-
-        if index < len(dataLeft):
+        if index >= (len(dataLeft) - 1):
+            break
+        if index <= len(dataLeft):
             index = index + 1
 
     print("index " + str(index))
@@ -35,4 +39,5 @@ while index < len(dataLeft):
 
     if index < len(dataLeft):
         index = index + 1
+
 
