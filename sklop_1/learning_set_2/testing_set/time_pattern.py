98ad83521fbc7c4de67faeb659c8717f998f6832
Amra <ao2282@student.uni-lj.si>
2018-03-11 01:37:02 +0100

    time pattern

--- sklop_1/learning_set_2/testing_set/time_pattern.py
+++ sklop_1/learning_set_2/testing_set/time_pattern.py
@@ -8,12 +8,23 @@ data = pd.read_csv('dataRight_testing.csv')
 #for index, row in dataLeft.iterrows():
 #print(dataLeft)
 data.index = pd.to_datetime(data['ts'])
-del data['ts']
+#del data['ts']
 #print(dataLeft)
 data2min = data.resample('2Min')
-data2minMin = data2min.max().dropna(how='any')
-data2minMax = data2min.max().dropna(how='any')
-data2minMean = data2min.mean().dropna(how='any')
+data2minMin = data2min['real_temp'].max().dropna(how='any')
+data2minMax = data2min['real_temp'].max().dropna(how='any')
+data2minMean = data2min['real_temp'].mean().dropna(how='any')
+
+#print("min")
+data2minMin = data2minMin.to_frame()
+#print(data2minMin)
+#print("max")
+data2minMax = data2minMax.to_frame()
+#print(data2minMax)
+#data2minMean = data2minMean.to_frame()
+
+result1 = pd.merge(data2minMin, data2minMax, on='ts', how='outer')
+print(result1)
 
 data3min = data.resample('3Min')
 data3minMin = data3min.min().dropna(how='any')
